063e69511c3bc4c73dc14e7b421a965e87652774
danzzen <yadavmohit05081999@gmail.com>
2017-08-27 00:16:52 +0530

    time

---
+++ app/src/main/java/com/myadav/fingerprintlock/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.myadav.fingerprintlock;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 27-08-2017.Mohit yadav
+ * settings will be defined here
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
